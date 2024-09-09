79b29aabcaf4067db69f1c34f559192034726f9e
Valentin Aebi <valentin.aebi@epfl.ch>
2024-09-09 22:56:01 +0200

    Add `buggy` method with null error and tricks

--- src/test/resources/testprojects/Vehicles/src/main/java/org/example/Main.java
+++ src/test/resources/testprojects/Vehicles/src/main/java/org/example/Main.java
@@ -12,4 +12,27 @@ public class Main {
         System.out.println("Profit: " + totalProfit);
     }
 
+    void buggy(String s, int x, int y) {
+        if (s == null) {
+            System.out.println("It's null");
+        }
+        if (x < y) {
+            System.out.println("Less than");
+            if (s != null) {
+                System.out.println(s.length());     // not an issue: s is checked right above
+            } else {
+                s = "Hello";    // (*)
+            }
+        } else if (x == y) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Greater than");
+            System.out.println(s.length());     // issue: example model = (s=null, x=1, y=0)
+        }
+        if (x + 2 < y){
+            // not an issue: x + 2 < y => x < y => s != null due to assignment at (*)
+            System.out.println(s.length());
+        }
+    }
+
 }

c49553604c3a5965b80d3c01ab08ff285355e879
Valentin Aebi <valentin.aebi@epfl.ch>
2024-09-10 17:40:04 +0200

    Create AdHocTestCases example directory

---
+++ src/test/resources/testprojects/TargetedTestCases/src/main/java/Foo.java
@@ -0,0 +1,28 @@
+
+public class Foo {
+
+    void foo(String s, int x, int y) {
+        if (s == null) {
+            System.out.println("It's null");
+        }
+        if (x < y) {
+            System.out.println("Less than");
+            if (s != null) {
+                System.out.println(s.length());     // not an issue: s is checked right above
+            } else {
+                s = "Hello";    // (*)
+            }
+        } else if (x == y) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Greater than");
+            System.out.println(s.length());     // #issue[INVK_NULL_REC]: example model = (s=null, x=1, y=0)
+            System.out.println(s.length());     // not an issue: cannot be executed due to previous line
+        }
+        if (x + 2 < y){
+            // not an issue: x + 2 < y => x < y => s != null due to assignment at (*)
+            System.out.println(s.length());
+        }
+    }
+
+}

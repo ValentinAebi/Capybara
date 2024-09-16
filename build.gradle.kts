1b9b50e55b67eb9202253763435ab5cad3a14158
Valentin Aebi <valentin.aebi@epfl.ch>
2024-09-16 22:16:06 +0200

    Remove unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,6 @@ dependencies {
     implementation("org.ow2.asm:asm-util:9.7")
     implementation("io.ksmt:ksmt-core:0.5.25")
     implementation("io.ksmt:ksmt-z3:0.5.25")
-    implementation("io.ksmt:ksmt-cvc5:0.5.25")
-    implementation("io.arrow-kt:arrow-core:1.2.4")
     testImplementation(kotlin("test"))
     testImplementation("org.apache.maven.shared:maven-invoker:3.3.0")
 }
